91ba7fd5a0b051db0b372ec0f1ed09c7476fc491
SahilGulati <sahilgulati102@gmail.com>
2020-04-26 20:21:00 +0530

    Added Docker file for JavaChromeDriver

---
+++ javachromedriver/chromedriver.sh
@@ -0,0 +1,5 @@
+wget https://chromedriver.storage.googleapis.com/81.0.4044.69/chromedriver_linux64.zip
+unzip chromedriver_linux64.zip
+mv chromedriver /usr/bin/chromedriver
+chown root:root /usr/bin/chromedriver
+chmod +x /usr/bin/chromedriver
