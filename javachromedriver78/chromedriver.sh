66d919fcd1546e9a67b9bb51d7ee93a91e3db86e
SahilGulati <sahilgulati102@gmail.com>
2020-05-02 21:23:45 +0530

    Added V78

---
+++ javachromedriver78/chromedriver.sh
@@ -0,0 +1,5 @@
+wget https://chromedriver.storage.googleapis.com/78.0.3904.105/chromedriver_linux64.zip
+unzip chromedriver_linux64.zip
+mv chromedriver /usr/bin/chromedriver
+chown root:root /usr/bin/chromedriver
+chmod +x /usr/bin/chromedriver
