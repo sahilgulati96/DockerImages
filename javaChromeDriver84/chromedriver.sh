46f92f3643286c8947f25dcfd9f233c45d380d1a
Sahil Gulati <sahilgulati102@gmail.com>
2020-07-18 20:57:30 +0530

    JavaChromeDriver84

---
+++ javaChromeDriver84/chromedriver.sh
@@ -0,0 +1,5 @@
+wget https://chromedriver.storage.googleapis.com/84.0.4147.30/chromedriver_linux64.zip
+unzip chromedriver_linux64.zip
+mv chromedriver /usr/bin/chromedriver
+chown root:root /usr/bin/chromedriver
+chmod +x /usr/bin/chromedriver
